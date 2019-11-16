6e46236f7abd36dee052b1866440ebdc95da66a9
Porkka Julius <julius.porkka@tieto.com>
2019-11-16 13:23:09 +0200

    Added exceptions for controller apis

---
+++ todo-app-service/src/main/java/com/todo/exception/ErrorMessages.java
@@ -0,0 +1,9 @@
+package com.todo.exception;
+
+public class ErrorMessages {
+  public static final String FORBIDDEN = "Forbidden";
+  public static final String NOT_FOUND = "Not found";
+  public static final String BAD_REQUEST = "Bad request";
+
+  private ErrorMessages() {}
+}
---
+++ todo-app-service/src/main/java/com/todo/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.todo.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class ErrorResponse {
+
+  private String code;
+  private String message;
+  private String info;
+}
