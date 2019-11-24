66f19e994df8958c2a0ed3a51f81ea18bd0ce485
Porkka Julius <julius.porkka@tieto.com>
2019-11-24 17:23:31 +0200

    Added tests for services and repositories

--- todo-app-service/src/test/java/com/todo/repository/TodoRepositoryTest.java
+++ todo-app-service/src/test/java/com/todo/repository/TodoRepositoryTest.java
@@ -1,5 +1,43 @@
 package com.todo.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.todo.model.Todo;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
 public class TodoRepositoryTest {
 
+  private static final String COLLECTION = "Todo";
+
+  Todo todo = new Todo();
+  List<Todo> todos = new ArrayList<>();
+
+  @Mock
+  private MongoTemplate mongoTemplate;
+  private TodoRepository todoRepository;
+
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    todoRepository = new TodoRepository(mongoTemplate);
+  }
+
+  @Test
+  public void saveTest() {
+    Mockito.when(mongoTemplate.save(todo, COLLECTION)).thenReturn(todo);
+    assertEquals (todo, todoRepository.save(todo));
+  }
+
+  @Test
+  public void findAllTest() {
+    Mockito.when(mongoTemplate.findAll(Todo.class, COLLECTION)).thenReturn(todos);
+    assertEquals (todos, todoRepository.findAll());
+  }
 }
--- todo-app-service/src/test/java/com/todo/service/TodoServiceTest.java
+++ todo-app-service/src/test/java/com/todo/service/TodoServiceTest.java
@@ -1,5 +1,67 @@
 package com.todo.service;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+
+import com.todo.dto.TodoDto;
+import com.todo.mapper.TodoMapper;
+import com.todo.model.Todo;
+import com.todo.repository.TodoRepository;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+@Slf4j
 public class TodoServiceTest {
 
+  private static final String PRIORITY = "Do it fast or wife doesn't like";
+
+  @Mock
+  private TodoRepository todoRepository;
+  private ModelMapper modelMapper;
+  private TodoMapper todoMapper;
+  private TodoService todoService;
+
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    modelMapper = new ModelMapper();
+    todoMapper = new TodoMapper(modelMapper);
+    todoService = new TodoService(todoRepository, todoMapper);
+  }
+
+  @Test
+  public void saveTest() {
+    Todo todo = Todo.builder()
+        .priority(PRIORITY)
+        .build();
+
+    TodoDto todoDto = TodoDto.builder()
+        .priority(PRIORITY)
+        .build();
+
+    Mockito.when(todoRepository.save(any(Todo.class))).thenReturn(todo);
+    TodoDto savedTodoDto = todoService.save(todoDto);
+    assertEquals(PRIORITY, savedTodoDto.getPriority());
+  }
+
+  @Test
+  public void findAllTest() {
+    // Create List with one object
+    List<Todo> todos = new ArrayList<>();
+    todos.add(Todo.builder()
+        .priority(PRIORITY)
+        .build());
+
+    Mockito.when(todoRepository.findAll()).thenReturn(todos);
+    List<TodoDto> todoDtos = todoService.findAll();
+    assertEquals(PRIORITY, todoDtos.get(0).getPriority());
+  }
 }
