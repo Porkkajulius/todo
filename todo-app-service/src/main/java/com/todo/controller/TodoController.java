f0c5151a085ee7786a2fe26410b762828ee1325f
Porkka Julius <julius.porkka@tieto.com>
2019-11-24 16:41:19 +0200

    Added POST API for todo

--- todo-app-service/src/main/java/com/todo/controller/TodoController.java
+++ todo-app-service/src/main/java/com/todo/controller/TodoController.java
@@ -9,12 +9,15 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +43,19 @@ public class TodoController {
   })
   @ResponseStatus(HttpStatus.OK)
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<List<TodoDto>> findTodos() {
+  public ResponseEntity<List<TodoDto>> findAll() {
     return ResponseEntity.ok(todoService.findTodos());
+  }
+
+  @ApiOperation("Save a new Todo")
+  @ApiResponses({
+      @ApiResponse(code = 201, message = "Todo was successfully created.", response = TodoDto.class),
+      @ApiResponse(code = 400, message = ErrorMessages.BAD_REQUEST, response = ErrorResponse.class),
+      @ApiResponse(code = 403, message = ErrorMessages.FORBIDDEN)
+  })
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<TodoDto> save(@RequestBody @Valid TodoDto todoDto) {
+    return new ResponseEntity<>(todoService.save(todoDto), HttpStatus.CREATED);
   }
 }
